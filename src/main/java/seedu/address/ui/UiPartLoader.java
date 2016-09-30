7f49e86569f311a598d3730df65b6e884710b29e
Paul Tan <pyokagan@gmail.com>
2016-09-30 18:06:25 +0800

    codestyle: Ensure modifiers are all in order
    The Java Language specification, sections 8.1.1, 8.3.1, 8.4.3 and 9.4
    recommend that modifiers be specified in this order:
    
    1. public
    2. protected
    3. private
    4. abstract
    5. default
    6. static
    7. final
    8. transient
    9. volatile
    10. synchronized
    11. native
    12. strictfp
    
    Fix violations of the above ordering.

--- src/main/java/seedu/address/ui/UiPartLoader.java
+++ src/main/java/seedu/address/ui/UiPartLoader.java
@@ -10,7 +10,7 @@ import seedu.address.MainApp;
  * A utility class to load UiParts from FXML files.
  */
 public class UiPartLoader {
-    private final static String FXML_FILE_FOLDER = "/view/";
+    private static final String FXML_FILE_FOLDER = "/view/";
 
     public static <T extends UiPart> T loadUiPart(Stage primaryStage, T controllerSeed) {
         return loadUiPart(primaryStage, null, controllerSeed);
