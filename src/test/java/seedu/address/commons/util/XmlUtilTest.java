6782648e79dc60446c7f4800bcaf2d55a9145828
Paul Tan <pyokagan@gmail.com>
2016-10-05 18:20:04 +0800

    testutil: rename AddressBookBuilder to TaskBookBuilder

--- src/test/java/seedu/address/commons/util/XmlUtilTest.java
+++ src/test/java/seedu/address/commons/util/XmlUtilTest.java
@@ -13,7 +13,7 @@ import org.junit.rules.ExpectedException;
 
 import seedu.address.model.TaskBook;
 import seedu.address.storage.XmlSerializableTaskBook;
-import seedu.address.testutil.AddressBookBuilder;
+import seedu.address.testutil.TaskBookBuilder;
 import seedu.address.testutil.TestUtil;
 
 public class XmlUtilTest {
@@ -85,7 +85,7 @@ public class XmlUtilTest {
         assertEquals((new TaskBook(dataToWrite)).toString(),(new TaskBook(dataFromFile)).toString());
         //TODO: use equality instead of string comparisons
 
-        AddressBookBuilder builder = new AddressBookBuilder(new TaskBook());
+        TaskBookBuilder builder = new TaskBookBuilder(new TaskBook());
         dataToWrite = new XmlSerializableTaskBook(builder.withPerson(TestUtil.generateSamplePersonData().get(0)).withTag("Friends").build());
 
         XmlUtil.saveDataToFile(TEMP_FILE, dataToWrite);
--- src/test/java/seedu/address/testutil/TaskBookBuilder.java
+++ src/test/java/seedu/address/testutil/TaskBookBuilder.java
@@ -9,22 +9,22 @@ import seedu.address.model.task.UniqueTaskList;
 /**
  * A utility class to help with building Addressbook objects.
  * Example usage: <br>
- *     {@code TaskBook ab = new AddressBookBuilder().withPerson("John", "Doe").withTag("Friend").build();}
+ *     {@code TaskBook ab = new TaskBookBuilder().withPerson("John", "Doe").withTag("Friend").build();}
  */
-public class AddressBookBuilder {
+public class TaskBookBuilder {
 
     private TaskBook addressBook;
 
-    public AddressBookBuilder(TaskBook addressBook) {
+    public TaskBookBuilder(TaskBook addressBook) {
         this.addressBook = addressBook;
     }
 
-    public AddressBookBuilder withPerson(Task person) throws UniqueTaskList.DuplicatePersonException {
+    public TaskBookBuilder withPerson(Task person) throws UniqueTaskList.DuplicatePersonException {
         addressBook.addPerson(person);
         return this;
     }
 
-    public AddressBookBuilder withTag(String tagName) throws IllegalValueException {
+    public TaskBookBuilder withTag(String tagName) throws IllegalValueException {
         addressBook.addTag(new Tag(tagName));
         return this;
     }
