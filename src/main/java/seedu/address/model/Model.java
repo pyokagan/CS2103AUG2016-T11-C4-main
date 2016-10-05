cbd87e7d1fad0354d944e77cebcaee7089dfcfd7
Paul Tan <pyokagan@gmail.com>
2016-10-05 18:45:48 +0800

    Model: rename updateFilteredPersonList() to updateFilteredTaskList()

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -29,7 +29,7 @@ public interface Model {
     /** Updates the filter of the filtered person list to show all persons */
     void updateFilteredListToShowAll();
 
-    /** Updates the filter of the filtered person list to filter by the given keywords*/
-    void updateFilteredPersonList(Set<String> keywords);
+    /** Updates the filter of the filtered task list to filter by the given keywords*/
+    void updateFilteredTaskList(Set<String> keywords);
 
 }
