b0f5fcb9ba92279434c4fbae9a41ba7cfd6be0ad
Paul Tan <pyokagan@gmail.com>
2016-10-05 19:21:04 +0800

    DeleteCommand: rename MESSAGE_DELETE_PERSON_SUCCESS to MESSAGE_DELETE_TASK_SUCCESS

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -17,7 +17,7 @@ public class DeleteCommand extends Command {
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Task: %1$s";
+    public static final String MESSAGE_DELETE_TASK_SUCCESS = "Deleted Task: %1$s";
 
     public final int targetIndex;
 
@@ -43,7 +43,7 @@ public class DeleteCommand extends Command {
             assert false : "The target person cannot be missing";
         }
 
-        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
+        return new CommandResult(String.format(MESSAGE_DELETE_TASK_SUCCESS, personToDelete));
     }
 
 }
