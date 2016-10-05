d054c9eba118f05f962207dc79c6b13ea2af2df4
Paul Tan <pyokagan@gmail.com>
2016-10-06 07:33:28 +0800

    config: rename default appTitle to "Task Tracker"
    This is more representative of what our application name is.
    
    NOTE: users with existing "config.json"s will have to delete their
    configuration files for this change to take effect.

--- src/main/java/seedu/address/commons/core/Config.java
+++ src/main/java/seedu/address/commons/core/Config.java
@@ -11,7 +11,7 @@ public class Config {
     public static final String DEFAULT_CONFIG_FILE = "config.json";
 
     // Config values customizable through config file
-    private String appTitle = "Address App";
+    private String appTitle = "Task Tracker";
     private Level logLevel = Level.INFO;
     private String userPrefsFilePath = "preferences.json";
     private String taskBookFilePath = "data/addressbook.xml";
