48ba55c668b7d38e84b49a84b970d2477b3b1c50
Paul Tan <pyokagan@gmail.com>
2016-10-06 21:46:48 +0800

    model: push down ReadOnlyTask.isSameStateAs() into Task.equals()
    We are going to merge ReadOnlyTask with Task in the future.
    
    In preparation of that, move ReadOnlyTask.isSameStateAs() into
    Task.equals().

--- src/main/java/seedu/address/model/task/ReadOnlyTask.java
+++ src/main/java/seedu/address/model/task/ReadOnlyTask.java
@@ -9,15 +9,6 @@ public interface ReadOnlyTask {
     Name getName();
 
     /**
-     * Returns true if both have the same state. (interfaces cannot override .equals)
-     */
-    default boolean isSameStateAs(ReadOnlyTask other) {
-        return other == this // short circuit if same object
-                || (other != null // this is first to avoid NPE below
-                && other.getName().equals(this.getName())); // state checks here onwards
-    }
-
-    /**
      * Formats the person as text, showing all contact details.
      */
     default String getAsText() {
--- src/main/java/seedu/address/model/task/Task.java
+++ src/main/java/seedu/address/model/task/Task.java
@@ -34,9 +34,9 @@ public class Task implements ReadOnlyTask {
 
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof ReadOnlyTask // instanceof handles nulls
-                && this.isSameStateAs((ReadOnlyTask) other));
+        return other == this
+               || (other instanceof ReadOnlyTask
+               && ((ReadOnlyTask)other).getName().equals(getName()));
     }
 
     @Override
