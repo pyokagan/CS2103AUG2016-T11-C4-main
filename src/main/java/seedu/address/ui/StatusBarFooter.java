a86993970e60f9dfefbb7a5a8838df00f5fa98c1
Paul Tan <pyokagan@gmail.com>
2016-10-09 12:18:47 +0800

    ui: simplify StatusBarFooter
    StatusBarFooter currently constructs and sets up its StatusBar controls
    using pure code.
    
    The StatusBar control is actually a real control that can be initialized
    directly in the FXML file. We can avoid placeholder elements and make
    things a lot simpler by just doing that.
    
    While we are here, also make StatusBarFooter more generic by returning a
    Pane as its root node rather than a GridPane. This allows us to use
    different kinds of panes in the future (e.g. HBox, VBox etc.) without
    needing to change the interface.

--- src/main/java/seedu/address/ui/StatusBarFooter.java
+++ src/main/java/seedu/address/ui/StatusBarFooter.java
@@ -8,59 +8,37 @@ import org.controlsfx.control.StatusBar;
 import com.google.common.eventbus.Subscribe;
 
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.model.TaskBookChangedEvent;
-import seedu.address.commons.util.FxViewUtil;
 
 /**
  * A ui for the status bar that is displayed at the footer of the application.
  */
-public class StatusBarFooter extends UiPart<GridPane> {
+public class StatusBarFooter extends UiPart<Pane> {
+
     private static final Logger logger = LogsCenter.getLogger(StatusBarFooter.class);
-    private StatusBar syncStatus;
-    private StatusBar saveLocationStatus;
-
-    @FXML
-    private AnchorPane saveLocStatusBarPane;
-
-    @FXML
-    private AnchorPane syncStatusBarPane;
 
     private static final String FXML = "/view/StatusBarFooter.fxml";
 
+    @FXML
+    private StatusBar syncStatus;
+
+    @FXML
+    private StatusBar saveLocationStatus;
+
     public StatusBarFooter(String saveLocation) {
         super(FXML);
-        addMainPane();
-        addSyncStatus();
         setSyncStatus("Not updated yet in this session");
-        addSaveLocation();
         setSaveLocation("./" + saveLocation);
-    }
-
-    private void addMainPane() {
-        FxViewUtil.applyAnchorBoundaryParameters(getRoot(), 0.0, 0.0, 0.0, 0.0);
     }
 
     private void setSaveLocation(String location) {
         this.saveLocationStatus.setText(location);
     }
 
-    private void addSaveLocation() {
-        this.saveLocationStatus = new StatusBar();
-        FxViewUtil.applyAnchorBoundaryParameters(saveLocationStatus, 0.0, 0.0, 0.0, 0.0);
-        saveLocStatusBarPane.getChildren().add(saveLocationStatus);
-    }
-
     private void setSyncStatus(String status) {
         this.syncStatus.setText(status);
-    }
-
-    private void addSyncStatus() {
-        this.syncStatus = new StatusBar();
-        FxViewUtil.applyAnchorBoundaryParameters(syncStatus, 0.0, 0.0, 0.0, 0.0);
-        syncStatusBarPane.getChildren().add(syncStatus);
     }
 
     @Subscribe
