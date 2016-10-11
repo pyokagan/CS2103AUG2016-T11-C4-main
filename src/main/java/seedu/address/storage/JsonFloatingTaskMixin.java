104616492862750b1381502fb2eb5a5e2e8d5280
nightism <smy961025@yeah.net>
2016-10-12 02:05:55 +0800

    storage: implement JsonPriorityMixin and JsonFloatingTaskMixin
    This teaches the JsonStorageModule how to serialize/deserialize Priority
    and FloatingTasks instances.

---
+++ src/main/java/seedu/address/storage/JsonFloatingTaskMixin.java
@@ -0,0 +1,16 @@
+package seedu.address.storage;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import seedu.address.model.task.Name;
+import seedu.address.model.task.Priority;
+
+@JsonPropertyOrder({"name", "priority"})
+public abstract class JsonFloatingTaskMixin {
+
+    JsonFloatingTaskMixin(@JsonProperty("name") Name name,
+            @JsonProperty("priority") Priority priority) {
+    }
+
+}
