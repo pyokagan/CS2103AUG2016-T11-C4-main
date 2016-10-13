fa76ec08e2d9d49acbbed480c391ac19f701e267
Paul Tan <pyokagan@gmail.com>
2016-10-13 09:02:01 +0800

    ui: implement DeadlineTaskListCard

--- src/main/java/seedu/address/ui/DeadlineTaskListPane.java
+++ src/main/java/seedu/address/ui/DeadlineTaskListPane.java
@@ -2,6 +2,7 @@ package seedu.address.ui;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
 import seedu.address.model.task.DeadlineTask;
@@ -16,6 +17,16 @@ public class DeadlineTaskListPane extends UiPart<Pane> {
     public DeadlineTaskListPane(ObservableList<DeadlineTask> deadlineTaskList) {
         super(FXML);
         deadlineTaskListView.setItems(deadlineTaskList);
+        deadlineTaskListView.setCellFactory(listView -> new DeadlineTaskListCell());
+    }
+
+    private static class DeadlineTaskListCell extends ListCell<DeadlineTask> {
+        @Override
+        protected void updateItem(DeadlineTask deadlineTask, boolean empty) {
+            super.updateItem(deadlineTask, empty);
+            final DeadlineTaskListCard card = new DeadlineTaskListCard(deadlineTask, getIndex() + 1);
+            setGraphic(card.getRoot());
+        }
     }
 
 }
