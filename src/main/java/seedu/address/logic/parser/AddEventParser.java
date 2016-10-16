7e73d2a0dd97b4a9298c22d34b700e4598ec0bd8
nightism <smy961025@yeah.net>
2016-10-16 23:18:58 +0800

    parser: simplify one if-else clause in AddEventParser

--- src/main/java/seedu/address/logic/parser/AddEventParser.java
+++ src/main/java/seedu/address/logic/parser/AddEventParser.java
@@ -98,11 +98,10 @@ public class AddEventParser {
         if (args.isEmpty()) {
             throw new IllegalValueException("expected ending time or ending date");
         }
-        if (!isKeywordTo(args.get(0))) {
-            throw new IllegalValueException("expected keyword \"to\"");
-        }
         if (isKeywordTo(args.get(0))) {
             args.remove(0);
+        } else {
+            throw new IllegalValueException("expected keyword \"to\"");
         }
 
         // endDate (optional)
