28fd6133dd200e4016887e62e06908f7b061b2a9
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2016-10-18 02:33:45 +0800

    Parser now recognises redo

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -26,6 +26,7 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.IncorrectCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
 
@@ -106,6 +107,9 @@ public class Parser {
             
         case UndoCommand.COMMAND_WORD:
         	return new UndoCommand();
+        	
+        case RedoCommand.COMMAND_WORD:
+        	return new RedoCommand();
 
         default:
             return new IncorrectCommand(MESSAGE_UNKNOWN_COMMAND);
