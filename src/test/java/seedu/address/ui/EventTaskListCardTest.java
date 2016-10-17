1b4aac5371a827c2835791ceac82b82fb085048b
Paul Tan <pyokagan@gmail.com>
2016-10-17 16:57:09 +0800

    ui: add EventTaskListCardTest

---
+++ src/test/java/seedu/address/ui/EventTaskListCardTest.java
@@ -0,0 +1,67 @@
+package seedu.address.ui;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+
+import javafx.scene.control.Label;
+import seedu.address.model.task.TypicalEventTasks;
+import seedu.address.testutil.GuiTests;
+
+@Category({GuiTests.class})
+public class EventTaskListCardTest extends FxRobot {
+
+    private TypicalEventTasks tet = new TypicalEventTasks();
+
+    private EventTaskListCard eventTaskListCard;
+
+    private Label indexLabel;
+
+    private Label nameLabel;
+
+    private Label startLabel;
+
+    private Label endLabel;
+
+    @BeforeClass
+    public static void setupFxToolkit() throws Exception {
+        FxToolkit.registerPrimaryStage();
+    }
+
+    private void setupNodes() throws Exception {
+        FxToolkit.showStage();
+        indexLabel = lookup("#indexLabel").query();
+        nameLabel = lookup("#nameLabel").query();
+        startLabel = lookup("#startLabel").query();
+        endLabel = lookup("#endLabel").query();
+    }
+
+    @Test
+    public void withEventTask_showsInfoInLabels() throws Exception {
+        FxToolkit.setupSceneRoot(() -> {
+            eventTaskListCard = new EventTaskListCard(tet.lunchWithBillGates, 42);
+            return eventTaskListCard.getRoot();
+        });
+        setupNodes();
+        assertEquals("42. ", indexLabel.getText());
+        assertEquals("Lunch with Bill Gates", nameLabel.getText());
+        assertEquals("1970-01-01T00:00", startLabel.getText());
+        assertEquals("1970-01-01T01:00", endLabel.getText());
+    }
+
+    @Test
+    public void nullEventTask_becomesInvisible() throws Exception {
+        FxToolkit.setupSceneRoot(() -> {
+            eventTaskListCard = new EventTaskListCard(null, 34);
+            return eventTaskListCard.getRoot();
+        });
+        setupNodes();
+        assertFalse(eventTaskListCard.getRoot().isVisible());
+    }
+
+}
