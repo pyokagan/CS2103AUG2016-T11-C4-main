a8205b48826b1c95276b18c413c25db8fbb7346a
nightism <smy961025@yeah.net>
2016-10-19 19:01:53 +0800

    Task: isFinished attribute added in DeadlineTask

--- src/main/java/seedu/address/model/task/DeadlineTask.java
+++ src/main/java/seedu/address/model/task/DeadlineTask.java
@@ -3,26 +3,48 @@ package seedu.address.model.task;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+import java.util.function.Predicate;
+
 import seedu.address.commons.exceptions.IllegalValueException;
 
 public class DeadlineTask extends Task {
 
-    private static final String FMT_STRING = "DeadlineTask[name=%s, due=%s]";
+    private static final String FMT_STRING = "DeadlineTask[name=%s, due=%s, finished=%s]";
 
     private final LocalDateTime due;
 
-    public DeadlineTask(Name name, LocalDateTime due) {
+    private boolean finished;
+
+    public static Predicate<? super DeadlineTask> isNotFinishedDeadline() {
+        return p -> !p.isFinished();
+    }
+
+    public DeadlineTask(Name name, LocalDateTime due, boolean finished) {
         super(name);
         assert due != null;
         this.due = due;
+        this.finished = finished;
+    }
+
+    public DeadlineTask(Name name, LocalDateTime due) {
+        this(name, due, false);
     }
 
     public DeadlineTask(String name, LocalDateTime due) throws IllegalValueException {
-        this(new Name(name), due);
+        this(new Name(name), due, false);
     }
 
     public LocalDateTime getDue() {
         return due;
+    }
+
+    public boolean isFinished() {
+        return this.finished;
+    }
+
+    public void markAsFinished() {
+        assert this.finished == false;
+        this.finished = true;
     }
 
     @Override
@@ -40,7 +62,7 @@ public class DeadlineTask extends Task {
 
     @Override
     public String toString() {
-        return String.format(FMT_STRING, name, due);
+        return String.format(FMT_STRING, name, due, finished);
     }
 
 }
