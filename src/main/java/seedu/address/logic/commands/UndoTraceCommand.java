ae53b57d44a1c5514e8fb65ea026be82445adf23
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2016-10-23 19:36:12 +0800

    create redo trace and undo trace Commands

---
+++ src/main/java/seedu/address/logic/commands/UndoTraceCommand.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.commands;
+
+public class UndoTraceCommand extends Command {
+    public static final String COMMAND_WORD = "undo trace";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Show a list of undoable actions\n\t" + "Example: " + COMMAND_WORD;
+
+    @Override
+    public CommandResult execute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
