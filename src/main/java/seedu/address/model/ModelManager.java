ed40fc6c890e9b810406c32ea08338db559c9671
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2016-10-23 16:33:27 +0800

    add comment for head

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -36,8 +36,8 @@ public class ModelManager extends ComponentManager implements Model {
     private final FilteredList<DeadlineTask> filteredDeadlineTasks;
 
     //for undo
-    private ArrayList<Commit> commits = new ArrayList<Commit>();//a stack of past TaskBook states
-    private int head;
+    private ArrayList<Commit> commits = new ArrayList<Commit>();
+    private int head; //head points to a the current commit which holds the TaskBook displayed by the UI
 
     /**
      * Initializes a ModelManager with the given config and TaskBook TaskBook
