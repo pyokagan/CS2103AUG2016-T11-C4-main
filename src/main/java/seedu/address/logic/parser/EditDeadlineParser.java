b9db3cdaf62203370604ab7814f01b86ebeb1d76
Paul Tan <pyokagan@gmail.com>
2016-10-24 18:50:02 +0800

    EditDeadlineParser: implement Parser interface
    This will later allow us to integrate this parser with the
    SubcommandParser in TaskTrackerParser.

--- src/main/java/seedu/address/logic/parser/EditDeadlineParser.java
+++ src/main/java/seedu/address/logic/parser/EditDeadlineParser.java
@@ -1,72 +1,52 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Optional;
 
-import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.commons.util.StringUtil;
-import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.EditDeadlineCommand;
-import seedu.address.logic.commands.IncorrectCommand;
+import seedu.address.logic.parser.CommandLineParser.Argument;
+import seedu.address.logic.parser.CommandLineParser.OptionalFlag;
+import seedu.address.model.task.Name;
 
-public class EditDeadlineParser {
-    private static final Pattern CMD_PATTERN = Pattern.compile("^(?<index>\\d+)"
-                                                               + "(\\s+dd-(?<newDate>[^\\s]+))?"
-                                                               + "(\\s+dt-(?<newTime>[^\\s]+))?"
-                                                               + "(\\s+n-(?<newName>.+))?"
-                                                               + "$");
+public class EditDeadlineParser implements Parser<EditDeadlineCommand> {
 
-    private final LocalDateTime referenceDateTime;
+    private final Argument<Integer> indexArg = new Argument<>("INDEX", new IndexParser());
+    private final OptionalFlag<LocalDate> newDateFlag = new OptionalFlag<>("dd-", "NEW_DUE_DATE", new DateParser());
+    private final OptionalFlag<LocalTime> newTimeFlag = new OptionalFlag<>("dt-", "NEW_DUE_TIME", new TimeParser());
+    private final OptionalFlag<Name> newNameFlag = new OptionalFlag<>("n-", "NEW_NAME", new NameParser());
+    private final CommandLineParser cmdParser = new CommandLineParser()
+                                                        .addArgument(indexArg)
+                                                        .putFlag(newDateFlag)
+                                                        .putFlag(newTimeFlag)
+                                                        .putFlag(newNameFlag);
+
+    private final Optional<LocalDateTime> referenceDateTime;
 
     public EditDeadlineParser() {
-        this(null);
+        this(Optional.empty());
     }
 
     public EditDeadlineParser(LocalDateTime referenceDateTime) {
+        this(Optional.of(referenceDateTime));
+    }
+
+    public EditDeadlineParser(Optional<LocalDateTime> referenceDateTime) {
         this.referenceDateTime = referenceDateTime;
     }
 
-    public Command parse(String args) {
-        final Matcher matcher = CMD_PATTERN.matcher(args.trim());
-        if (!matcher.matches()) {
-            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditDeadlineCommand.MESSAGE_USAGE));
-        }
+    @Override
+    public EditDeadlineCommand parse(String str) throws ParseException {
+        // Tell date/time parsers the current time
+        final LocalDateTime now = referenceDateTime.orElse(LocalDateTime.now());
+        newDateFlag.setParser(new DateParser(now.toLocalDate()));
+        newTimeFlag.setParser(new TimeParser(now.toLocalTime()));
 
-        final String indexString = matcher.group("index").trim();
-        if (!StringUtil.isUnsignedInteger(indexString)) {
-            return new IncorrectCommand(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditDeadlineCommand.MESSAGE_USAGE));
-        }
-        final int index = Integer.parseInt(indexString);
+        cmdParser.parse(str);
 
-        final DateTimeParser parser = referenceDateTime != null ? new DateTimeParser(referenceDateTime)
-                                                                : new DateTimeParser();
-        LocalDate newDate = null;
-        LocalTime newTime = null;
-
-        String newName = matcher.group("newName");
-        try {
-            if (matcher.group("newDate") != null) {
-                newDate = parser.parseDate(matcher.group("newDate"));
-            }
-            if (matcher.group("newTime") != null) {
-                newTime = parser.parseTime(matcher.group("newTime"));
-            }
-
-        } catch (IllegalValueException e) {
-            return new IncorrectCommand(e.getMessage());
-        }
-
-        try {
-            return new EditDeadlineCommand(index, newName, newDate, newTime);
-        } catch (IllegalValueException e) {
-            return new IncorrectCommand(e.getMessage());
-        }
+        return new EditDeadlineCommand(indexArg.getValue(), newNameFlag.getValue(), newDateFlag.getValue(),
+                                       newTimeFlag.getValue());
     }
 
 }
