d543babb484e0c674172b758ba6e7c5bbf61aeb4
Paul Tan <pyokagan@gmail.com>
2016-10-27 00:52:20 +0800

    EventTaskListCard: strikethrough text when task is finished

--- src/main/java/seedu/address/ui/EventTaskListCard.java
+++ src/main/java/seedu/address/ui/EventTaskListCard.java
@@ -1,5 +1,7 @@
 package seedu.address.ui;
 
+import java.time.LocalDateTime;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
@@ -32,6 +34,9 @@ public class EventTaskListCard extends UiPart<Pane> {
             nameLabel.setText(eventTask.getName().toString());
             startLabel.setText(eventTask.getStart().toString());
             endLabel.setText(eventTask.getEnd().toString());
+            if (eventTask.getEnd().isBefore(LocalDateTime.now())) {
+                getRoot().getStyleClass().add("finished");
+            }
         } else {
             getRoot().setVisible(false);
         }
