ef1fea7c88e8db875dcb266ab592513267f20c19
Paul Tan <pyokagan@gmail.com>
2016-10-31 22:05:56 +0800

    CommandBox: simplify code
    Now that we do not have any "incorrect command executed" event to care
    about, the command box code can be greatly simplified to not do the
    weird contrived "clear the command text box but save its contents and
    restore it when we have an error" dance.

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -13,19 +13,16 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.parser.ParseException;
 
 public class CommandBox extends UiPart<Pane> {
-
     private final Logger logger = LogsCenter.getLogger(CommandBox.class);
 
     private static final String FXML = "/view/CommandBox.fxml";
 
     private final ResultDisplay resultDisplay;
-    String previousCommandTest;
 
     private final Logic logic;
 
     @FXML
     private TextField commandTextField;
-    private CommandResult mostRecentResult;
 
     private OnCommandResultCallback onCommandResultCallback;
 
@@ -50,23 +47,17 @@ public class CommandBox extends UiPart<Pane> {
 
     @FXML
     private void handleCommandInputChanged() {
-        //Take a copy of the command text
-        previousCommandTest = commandTextField.getText();
-
-        /* We assume the command is correct. If it is incorrect, the command box will be changed accordingly
-         * in the event handling code {@link #handleIncorrectCommandAttempted}
-         */
-        setStyleToIndicateCorrectCommand();
         try {
-            mostRecentResult = logic.execute(previousCommandTest);
-            resultDisplay.postMessage(mostRecentResult.feedbackToUser);
-            logger.info("Result: " + mostRecentResult.feedbackToUser);
+            final CommandResult result = logic.execute(commandTextField.getText());
+            logger.info("Result: " + result.feedbackToUser);
+            resultDisplay.postMessage(result.feedbackToUser);
+            setStyleToIndicateCorrectCommand();
             if (onCommandResultCallback != null) {
-                onCommandResultCallback.call(mostRecentResult);
+                onCommandResultCallback.call(result);
             }
         } catch (ParseException | CommandException e) {
+            logger.info("Result: " + e.toString());
             setStyleToIndicateIncorrectCommand();
-            restoreCommandText();
             resultDisplay.postMessage(e.getMessage());
         }
     }
@@ -77,13 +68,6 @@ public class CommandBox extends UiPart<Pane> {
     private void setStyleToIndicateCorrectCommand() {
         commandTextField.getStyleClass().remove("error");
         commandTextField.setText("");
-    }
-
-    /**
-     * Restores the command box text to the previously entered command
-     */
-    private void restoreCommandText() {
-        commandTextField.setText(previousCommandTest);
     }
 
     /**
