4c0e359c88bc2a26e1a9fce667c2add0c53a5d12
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2016-11-04 23:05:01 +0800

    add font

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -17,20 +17,20 @@ public class HelpWindow {
 
     private static final String USERGUIDE_URL = "https://github.com/CS2103AUG2016-T11-C4/main/blob/master/docs/UserGuide.md";
     private static final String HELPBOX_CONTENT =
-              "Add Floating Task             | add \"FLOATING_TASK_NAME\"  [p-PRIORITY]\n"
-            + "Add Deadline Task             | add \"DEADLINE_NAME\" <DATE> <TIME>\n"
-            + "Add Events                    | add \"EVENT_NAME\" <STARTING_DATE> <STARTING_TIME> to <ENDING_DATE> <ENDING_TIME>\n"
-            + "Delete                          | del <INDEX>\n"
-            + "Edit Floating Tasks           | edit <INDEX> [n-NEW_NAME] [p-PRIORITY]\n"
-            + "Edit Deadline                | edit <INDEX> [dd-DUE_DATE] [dt-DUE_TIME] [n-NEW_NAME]\n"
-            + "Mark task as finished      | fin <INDEX>\n"
+              "Add Floating Task         | add \"FLOATING_TASK_NAME\"  [p-PRIORITY]\n"
+            + "Add Deadline Task         | add \"DEADLINE_NAME\" <DATE> <TIME>\n"
+            + "Add Events                | add \"EVENT_NAME\" <STARTING_DATE> <STARTING_TIME> to <ENDING_DATE> <ENDING_TIME>\n"
+            + "Delete                    | del <INDEX>\n"
+            + "Edit Floating Tasks       | edit <INDEX> [n-NEW_NAME] [p-PRIORITY]\n"
+            + "Edit Deadline             | edit <INDEX> [dd-DUE_DATE] [dt-DUE_TIME] [n-NEW_NAME]\n"
+            + "Mark task as finished     | fin <INDEX>\n"
             + "Mark a task as unfinished | unfin <INDEX>\n"
-            + "Search for keywords         | search KEYWORD\n"
-            + "Undo                             | undo\n"
-            + "Redo                              | redo\n"
-            + "Clear                              | clear\n"
-            + "Exit                                 | exit\n"
-            + "Hide and show Task Tracker  | Ctrl + SPACE\n\n"
+            + "Search for keywords       | search KEYWORD\n"
+            + "Undo                      | undo\n"
+            + "Redo                      | redo\n"
+            + "Clear                     | clear\n"
+            + "Exit                      | exit\n"
+            + "Hide and show Task Tracker| Ctrl + SPACE\n\n"
             + "For full guide: " + USERGUIDE_URL;
 
     private static final String ICON = "/images/help_icon.png";
@@ -55,6 +55,8 @@ public class HelpWindow {
         helpBox.setResizable(true);
         helpBox.getDialogPane().setPrefWidth(700.0);
         helpBox.getDialogPane().setPrefHeight(340.0);
+        helpBox.getDialogPane().getStylesheets().add(getClass().getResource("view/DarkTheme.css").toExternalForm());
+        helpBox.getDialogPane().getStyleClass().add("helpBox");
 
         //set contents
         helpBox.setTitle("HELP");
