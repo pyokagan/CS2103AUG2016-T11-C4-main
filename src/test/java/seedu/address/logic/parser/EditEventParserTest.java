ae460fce6e769f3a2e9f1bfe667667de518ac83d
Paul Tan <pyokagan@gmail.com>
2016-11-05 20:55:08 +0800

    EditEventParserTest: increase test coverage

--- src/test/java/seedu/address/logic/parser/EditEventParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditEventParserTest.java
@@ -7,6 +7,8 @@ import static seedu.address.testutil.TestUtil.assertThrows;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Optional;
 
 import org.junit.Before;
@@ -14,14 +16,21 @@ import org.junit.Test;
 
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.EditEventCommand;
+import seedu.address.model.ModelManager;
+import seedu.address.model.task.TypicalEventTasks;
 
 public class EditEventParserTest {
+
+    private final TypicalEventTasks tet = new TypicalEventTasks();
+
+    private final ModelManager model = new ModelManager();
 
     private EditEventParser parser;
 
     @Before
     public void setupParser() {
         parser = new EditEventParser(UNIX_EPOCH);
+        assertEquals(1, model.addEventTask(tet.lunchWithBillGates));
     }
 
     @Test
@@ -62,6 +71,46 @@ public class EditEventParserTest {
 
     private void assertIncorrect(String args) {
         assertThrows(ParseException.class, () -> parser.parse(args));
+    }
+
+    @Test
+    public void autocompleteName_returnsEventTaskName() {
+        assertEquals(Arrays.asList("Lunch with Bill Gates"), parser.autocomplete(model, "e1 n- ", 5));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "e2 n- ", 5));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "e1 n-a", 5));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "1 n-", 4));
+    }
+
+    @Test
+    public void autocompleteStartDate_returnsEventStartDate() {
+        assertEquals(Arrays.asList("1/1/1970"), parser.autocomplete(model, "e1 sd- ", 6));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "e2 sd- ", 6));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "e1 sd-4/5", 6));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "1 sd-", 5));
+    }
+
+    @Test
+    public void autocompleteStartTime_returnsEventStartTime() {
+        assertEquals(Arrays.asList("12am"), parser.autocomplete(model, "e1 st- ", 6));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "e2 st- ", 6));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "e1 st-4am", 6));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "1 st-", 5));
+    }
+
+    @Test
+    public void autocompleteEndDate_returnsEventEndDate() {
+        assertEquals(Arrays.asList("1/1/1970"), parser.autocomplete(model, "e1 ed- ", 6));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "e2 ed- ", 6));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "e1 ed-4/5", 6));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "1 ed-", 5));
+    }
+
+    @Test
+    public void autocompleteEndTime_returnsEventEndTime() {
+        assertEquals(Arrays.asList("1am"), parser.autocomplete(model, "e1 et- ", 6));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "e2 et- ", 6));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "e1 et-4am", 6));
+        assertEquals(Collections.emptyList(), parser.autocomplete(model, "1 et-", 5));
     }
 
 }
