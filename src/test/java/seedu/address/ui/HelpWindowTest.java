0bbe13876eaa8b0ae37fdc5431451bd616106102
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2016-11-06 19:34:17 +0800

    finish GUI testing

--- src/test/java/seedu/address/ui/HelpWindowTest.java
+++ src/test/java/seedu/address/ui/HelpWindowTest.java
@@ -13,12 +13,14 @@ import org.testfx.api.FxToolkit;
 
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
+import javafx.scene.web.WebView;
 import seedu.address.testutil.GuiTests;
 
 @Category({GuiTests.class})
 public class HelpWindowTest extends FxRobot {
 
     private HelpWindow helpWindow;
+    private WebView webView;
 
     @BeforeClass
     public static void setupFxToolkit() throws Exception {
@@ -32,6 +34,7 @@ public class HelpWindowTest extends FxRobot {
             return helpWindow.getStage();
         });
         FxToolkit.showStage();
+        //webView = lookup("#webView").query();
     }
 
     @After
@@ -43,6 +46,12 @@ public class HelpWindowTest extends FxRobot {
     public void constructor() {
         clickOn(helpWindow.getStage(), MouseButton.PRIMARY);
         press(KeyCode.ENTER);
+        release(KeyCode.ENTER);
+        webView = lookup("#webView").query();
+        assertEquals("https://github.com/CS2103AUG2016-T11-C4/main/blob/master/docs/UserGuide.md", webView.getEngine().getLocation());
+        sleep(3000);
+        clickOn(8000, 0, MouseButton.PRIMARY);
+        sleep(1000);
         press(KeyCode.ESCAPE);
         release(KeyCode.ESCAPE);
     }
