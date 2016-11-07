0953eb5eb35fd5943121035dbd9e0ac7d704c3a2
nightism <smy961025@yeah.net>
2016-11-07 20:37:08 +0800

    Test: editDeadlineCommand test code

--- src/test/java/seedu/address/logic/commands/EditDeadlineCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditDeadlineCommandTest.java
@@ -37,4 +37,38 @@ public class EditDeadlineCommandTest {
         Mockito.verify(model).setDeadlineTask(1, expected);
     }
 
+    @Test
+    public void execute_withNewName_replacesName() throws Exception {
+        final DeadlineTask expected = new DeadlineTask(new Name("Finish cs2013 project"), UNIX_EPOCH.plusDays(1), true);
+        final EditDeadlineCommand cmd = new EditDeadlineCommand(1, Optional.of(new Name("Finish cs2013 project")),
+                                                          Optional.empty(), Optional.empty());
+        final CommandResult result = cmd.execute(model);
+        assertEquals("Edited deadline task d1.", result.feedbackToUser);
+        Mockito.verify(model).setDeadlineTask(1, expected);
+    }
+
+    @Test
+    public void execute_withNewDueDate_replacesDueDate() throws Exception {
+        final DeadlineTask expected = new DeadlineTask(new Name("Finish project"),
+                                                       UNIX_EPOCH.plusDays(2), true);
+        final EditDeadlineCommand cmd = new EditDeadlineCommand(1, Optional.empty(),
+                                                          Optional.of(UNIX_EPOCH.plusDays(2).toLocalDate()),
+                                                          Optional.empty());
+        final CommandResult result = cmd.execute(model);
+        assertEquals("Edited deadline task d1.", result.feedbackToUser);
+        Mockito.verify(model).setDeadlineTask(1, expected);
+    }
+
+    @Test
+    public void execute_withNewDueTime_replacesDueTime() throws Exception {
+        final DeadlineTask expected = new DeadlineTask(new Name("Finish project"),
+                                                       UNIX_EPOCH.plusDays(1).plusMinutes(10),
+                                                       true);
+        final EditDeadlineCommand cmd = new EditDeadlineCommand(1, Optional.empty(), Optional.empty(),
+                                                                Optional.of(UNIX_EPOCH.plusDays(1).plusMinutes(10).toLocalTime()));
+        final CommandResult result = cmd.execute(model);
+        assertEquals("Edited deadline task d1.", result.feedbackToUser);
+        Mockito.verify(model).setDeadlineTask(1, expected);
+    }
+
 }
